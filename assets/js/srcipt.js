3d5abf1854bb89fe331b92b2cb7b683a9483a244
Rafael Loureiro <rafanetmello@gmail.com>
2022-09-29 11:05:31 -0300

    upload events

--- assets/js/srcipt.js
+++ assets/js/srcipt.js
@@ -1,5 +1,33 @@
 
+let slidePosition = document.querySelector('.banner').value;
 
+
+//eventes
+//add event pointer bunner
+document.querySelector('.pointer1').addEventListener('click', () => {
+    let slidePosition = document.querySelector('.slidearea');
+    if (slidePosition === -200 || slidePosition !== -400) {
+        slidePosition.style.marginLeft = '0vw';
+    }
+});
+
+document.querySelector('.pointer2').addEventListener('click', () => {
+    let slidePosition = document.querySelector('.slidearea');
+    if (slidePosition !== 0 || slidePosition !== -400) {
+        slidePosition.style.marginLeft = '-85vw';
+    }
+});
+document.querySelector('.pointer3').addEventListener('click', () => {
+    let slidePosition = document.querySelector('.slidearea');
+    if (slidePosition !== 0 || slidePosition !== -200) {
+        slidePosition.style.marginLeft = '-400vw';
+    }
+});
+
+
+
+
+//function menu
 window.onload = function () {
     document.querySelector(".menu").addEventListener("click", function () {
         if (document.querySelector(".menu nav ").style.display == 'flex') {
@@ -9,3 +37,5 @@ window.onload = function () {
         }
     });
 }
+
+
